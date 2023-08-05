dab2fa6225a81f843588fd9b0a4210f56969072a
otokki2100 <otokki2100@gmail.com>
2023-08-05 20:44:28 +0900

    update

--- dokuwiki/simple/terraform/tera_output.tf
+++ dokuwiki/simple/terraform/tera_output.tf
@@ -1,23 +1,23 @@
-output "vm_id" {
+output "domain" {
+  value = local.domain
+}
+
+output "vm_info_id" {
   value = module.ec2.id
 }
 
-output "vm_ip" {
-  value = module.ec2.public_ip
-}
-
-output "instance_type" {
+output "vm_info_instance_type" {
   value = var.ec2.instance_type
 }
 
-output "vm_user" {
+output "vm_ssh_1_user" {
   value = var.ec2.user
 }
 
-output "vm_z_ssh" {
-  value = "ssh ${var.ec2.user}@${module.ec2.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
+output "vm_ssh_2_ip" {
+  value = module.ec2.public_ip
 }
 
-output "domain" {
-  value = local.domain
+output "vm_ssh_3_ssh" {
+  value = "ssh ${var.ec2.user}@${module.ec2.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
 }
